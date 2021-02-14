b4fb89bd27207b2a927851729b415990c69e02d2
kennedyaquino <kennedy.eletronica.94@gmail.com>
2021-02-14 04:48:12 -0300

    saving images on aws s3

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -1,14 +1,34 @@
 const multer = require("multer");
 const path = require("path");
 const crypto = require("crypto");
+const multerS3 = require("multer-s3");
+const aws = require("aws-sdk");
 
-module.exports = {
-  dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
-  storage: multer.diskStorage({
+const storagetypes = {
+  local: multer.diskStorage({
     destination: (req, res, callback) => {
       callback(null, path.resolve(__dirname, "..", "..", "tmp", "uploads"));
     },
     filename: (req, file, callback) => {
+      crypto.randomBytes(16, (error, hash) => {
+        if (error) callback(error);
+
+        file.key = `${hash.toString("hex")}-${file.originalname}`;
+
+        callback(null, file.key);
+      });
+    },
+  }),
+  s3: multerS3({
+    s3: new aws.S3({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      region: process.env.AWS_DEFAULT_REGION,
+    }),
+    bucket: "upload-example-nodejs",
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: "public-read",
+    key: (req, file, callback) => {
       crypto.randomBytes(16, (error, hash) => {
         if (error) callback(error);
 
@@ -18,6 +38,11 @@ module.exports = {
       });
     },
   }),
+};
+
+module.exports = {
+  dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
+  storage: storagetypes[process.env.STORAGE_TYPE],
   limits: {
     fileSize: 2 * 1024 * 1024,
   },
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,15 +4,30 @@ const multerConfig = require("./config/multer");
 
 const Post = require("./models/Post");
 
+routes.get("/posts", async (req, res) => {
+  const posts = await Post.find();
+  return res.json(posts);
+});
+
 routes.post("/posts", multer(multerConfig).single("file"), async (req, res) => {
+  const { originalname: name, size, key, location: url = "" } = req.file;
+
   const post = await Post.create({
-    name: req.file.originalname,
-    size: req.file.size,
-    key: req.file.filename,
-    url: "",
+    name,
+    size,
+    key,
+    url,
   });
 
   return res.json(post);
+});
+
+routes.delete("/posts/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+
+  await post.remove();
+
+  return res.send();
 });
 
 module.exports = routes;
